ecf7a853deadaf85dad2177fbe69cd3e61563406
Stefan Ekwall <stefan.ekwall@pm.me>
2025-07-31 01:03:11 +0200

    fix: scroll user message to top on submit v19b

--- components/simple-chat-interface.tsx
+++ components/simple-chat-interface.tsx
@@ -2010,17 +2010,17 @@ const SimpleChatInterface = forwardRef<ChatInterfaceHandle, SimpleChatInterfaceP
                 // Desktop only - use piecewise function
                 if (containerHeight >= 500) {
                     const base1 = 15;
-                    const scale1 = 6000;
+                    const scale1 = 1000;
                     const power1 = 0.7;
                     topPadding = base1 + (scale1 / Math.pow(containerHeight, power1));
                 } else if (containerHeight >= 300) {
                     const base2 = 20;
-                    const scale2 = 7000;
+                    const scale2 = 1000;
                     const power2 = 0.8;
                     topPadding = base2 + (scale2 / Math.pow(containerHeight, power2));
                 } else {
                     const base3 = 15;
-                    const scale3 = 7000;
+                    const scale3 = 1000;
                     const power3 = 0.9;
                     topPadding = base3 + (scale3 / Math.pow(containerHeight, power3));
                 }
